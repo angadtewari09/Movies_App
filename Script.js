bb75ca62fc776e731f98be42e61d3cb369e32b85
Angad Tewari <54849378+angadtewari09@users.noreply.github.com>
2023-07-21 17:47:56 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,84 +5,27 @@
                     .then( (response) => { console.log(Object.values(response.cast[i++]) )     })
                     .catch(err => console.error(err)) 
 */
-const genre = [
-    {
-      "id": 28,
-      "name": "Action"
-    },
-    {
-      "id": 12,
-      "name": "Adventure"
-    },
-    {
-      "id": 16,
-      "name": "Animation"
-    },
-    {
-      "id": 35,
-      "name": "Comedy"
-    },
-    {
-      "id": 80,
-      "name": "Crime"
-    },
-    {
-      "id": 99,
-      "name": "Documentary"
-    },
-    {
-      "id": 18,
-      "name": "Drama"
-    },
-    {
-      "id": 10751,
-      "name": "Family"
-    },
-    {
-      "id": 14,
-      "name": "Fantasy"
-    },
-    {
-      "id": 36,
-      "name": "History"
-    },
-    {
-      "id": 27,
-      "name": "Horror"
-    },
-    {
-      "id": 10402,
-      "name": "Music"
-    },
-    {
-      "id": 9648,
-      "name": "Mystery"
-    },
-    {
-      "id": 10749,
-      "name": "Romance"
-    },
-    {
-      "id": 878,
-      "name": "Science Fiction"
-    },
-    {
-      "id": 10770,
-      "name": "TV Movie"
-    },
-    {
-      "id": 53,
-      "name": "Thriller"
-    },
-    {
-      "id": 10752,
-      "name": "War"
-    },
-    {
-      "id": 37,
-      "name": "Western"
-    }
-  ]
+const genre = [ 
+    { "id": 28, "name": "Action" }, 
+    { "id": 12, "name": "Adventure" }, 
+    { "id": 16, "name": "Animation" }, 
+    { "id": 35, "name": "Comedy" }, 
+    { "id": 80, "name": "Crime" }, 
+    { "id": 99, "name": "Documentary" }, 
+    { "id": 18, "name": "Drama" }, 
+    { "id": 10751, "name": "Family" }, 
+    { "id": 14, "name": "Fantasy" }, 
+    { "id": 36, "name": "History" }, 
+    { "id": 27, "name": "Horror" }, 
+    { "id": 10402, "name": "Music" }, 
+    { "id": 9648, "name": "Mystery" }, 
+    { "id": 10749, "name": "Romance" }, 
+    { "id": 878, "name": "Science Fiction" }, 
+    { "id": 10770, "name": "TV Movie" }, 
+    { "id": 53, "name": "Thriller" }, 
+    { "id": 10752, "name": "War" }, 
+    { "id": 37, "name": "Western" } 
+]
 const api_key = "api_key=e296ec8b756d3d400034f9379f43d92f";
 const base_url = "https://api.themoviedb.org/3";
 const dicovery = "/discover/movie?sort_by=popularity.desc&";
@@ -98,6 +41,9 @@ const previous = document.getElementById(`prev`);
 const next = document.getElementById('next');
 const current = document.getElementById(`current`);
 
+const className = "buttonName"; 
+const castName = "castName";
+const trailer = "trailerName"
 
 
 var currentPage = 0;
@@ -110,16 +56,48 @@ getMovies(api_url);
 function getMovies(api_url) {
     prevUrl = api_url;  
         fetch(api_url)
-        .then(function(response) { 
-            return response.json()
-        })
-        .then(function (data){
-           
-            //console.log(data.results);
-            if(data.results.length !== 0){
-                showMovies(data.results);
+        .then(res => res.json() )
+        .then( (data) => {
+
+            if(data.results.length !== 0) {
+                info.innerHTML = ``;
+                data.results.forEach(movie => {
+                    const {title,poster_path,vote_average,overview} = movie;
+                    const movie_element = document.createElement('div');
+                    movie_element.classList.add('movie');
+                    movie_element.innerHTML = `
+                        <img src="${image_base_url + poster_path}" alt="${movie.title}">
+                        <div class="movie-info">
+                        
+                            <h3>${TitleConcatinate(title)}</h3>
+                            <span class="${getcolor(vote_average)}" id="green"><i class="fas fa-star"></i>${movie.vote_average}</span>
+                        </div>
+                        <div class="overview">
+                            <h1>${title}</h1>
+                            <br>
+                            <h2> Release Date:</h2>  <p>${movie.release_date}</p>
+                            <br>
+                            <h2>Genre:</h2>  <p>${ getGenreList( movie.genre_ids )}</p>
+                            <br>
+                            <h2> Rating:</h2>  <p>${(vote_average).toFixed(2)}</p>
+                            <br>
+                            <h2>Director: </h2>  <p class=${castName} id=${movie.id*10} > </p>
+                            <br>
+                            <h2>Producer: </h2>  <p class=${castName} id=${movie.id*100} > </p>
+                            <br>
+                            <h2>Cast:</h2>  <p class=${castName} id=${movie.id} > ${getCast(movie.id)} </p>
+                            <br>
+                            <h2>Plot:</h2>
+                            <p>${movie.overview}</p>
+                            <br>
+                            <h2>Trailer:</h2>  <a id=${movie.vote_count} target="_blank" >${ getTrailer(movie.id , movie.vote_count) }</a>
+                            <br>
+                        </div>
+                    `;
+                    info.appendChild(movie_element);
+                });
+
                 currentPage = data.page;
-                //console.log(data.total_pages)
                 totalPages = data.total_pages;
                 nextPage = currentPage + 1;
                 previousPage = currentPage - 1;  
@@ -140,46 +118,83 @@ function getMovies(api_url) {
                 info.innerHTML= `<h1 class="no-reults">No Results Found<h1>`;
             }
         })
-        .catch(error => console.log(error))
+        .catch(error => console.error(error))
 }
-/*
-getMovies(api_url);
-function getMovies(url) {
-    fetch(url).then(response => response.json()).then(data => {
-        console.log(data.results);
-        showMovies(data.results);
-    })
-    
-}*/
 
- function showMovies(data) {
-    info.innerHTML = ``;
-    data.forEach(movie => {
-        
-        const {title,poster_path,vote_average,overview} = movie;
-        const movie_element = document.createElement('div');
-        movie_element.classList.add('movie');
-        movie_element.innerHTML = `
-            <img src="${image_base_url + poster_path}" alt="${movie.title}">
-            <div class="movie-info">
-            
-                <h3>${TitleConcatinate(title)}</h3>
-                <span class="${getcolor(vote_average)}" id="green"><i class="fas fa-star"></i>${movie.vote_average}</span>
-            </div>
-            <div class="overview">
-                <h1>${title}</h1>
-                <br>
-                <h2> Release Date:</h2>  <p>${movie.release_date}</p>
-                <h2>Genre:</h2>  <p>${ getGenreList( movie.genre_ids )}</p>
-                <h2> Rating:</h2>  <p>${(vote_average).toFixed(2)}</p>
-                <h2>Cast:</h2>  <p>${getCast(movie.id)}</p>
-                <h2>Plot:</h2>
-                <p>${movie.overview}</p>
-            </div>
-        `;
-        info.appendChild(movie_element);
-    });
- }
+let names;
+let director;
+
+function getCast( movie_id )
+{
+  fetch(`https://api.themoviedb.org/3/movie/${movie_id}/credits?language=en-US&api_key=e296ec8b756d3d400034f9379f43d92f`).then( res => res.json() )
+  .then( data=>{
+
+    let d = [];  
+    data.crew.map((ele , idx ) => { 
+      if( ele.department == "Directing" ) {
+        d.push( ele.name )
+      }
+    })
+    director = d.join(', ')
+    document.getElementById(movie_id*10).innerHTML = `${director}.`
+    
+    let p = []
+    data.crew.map((ele) => { 
+      if( ele.department == "Production") {
+        p.push( ele.name )
+      }
+    })
+    let producer = ""; 
+    p.forEach( (name , idx ) => {
+      if( idx <3) {
+        if( producer.length == 0 ) {
+          producer = producer + name;
+        }
+        else {
+          producer = producer + ", " + name
+        }
+         
+      }
+    })
+    document.getElementById(movie_id*100).innerHTML = `${producer}.`
+
+    if( data ) {
+      if( data.cast.length > 0 ){
+        var embed = []
+        data.cast.forEach( (element , num ) => 
+        {
+            if( num < 13 )
+            embed.push( element.name )
+        });
+        names = embed.join(', ')
+        document.getElementById(movie_id).innerHTML= ` ${names}, ...and others`
+      }
+    }
+  })
+}
+
+function getTrailer(id , vc)
+{
+  let trailer = `https://www.youtube.com/watch?v=`;
+  
+   fetch(`http://api.themoviedb.org/3/movie/${id}/videos?api_key=e296ec8b756d3d400034f9379f43d92f`)
+   .then( res => res.json() )
+   .then( (data) => {
+    if(data) {
+
+      if( data.results.length > 0 ) {
+        let tr = []
+        data.results.forEach((element , index) => {
+          tr.push( element.key )
+        })
+        document.getElementById(vc).href =`${trailer}${tr[ 0 ]}`;
+        document.getElementById(vc).innerHTML =`${trailer}${tr[0]}`
+      }
+    }
+   })
+   .catch( err => console.log( err ))
+}
+
 
  function TitleConcatinate(title) {
    
@@ -247,18 +262,17 @@ prev.addEventListener('click', () =>{
     }
 })
 
+
 //Show the next Page.
-function ShowNextPage(page) {
-    //console.log('hello best friend')
-   
-document.getElementById("current").innerText = page
+function ShowNextPage(page) 
+{   
+  document.getElementById("current").innerText = page
   let urlsplit = prevUrl.split('?'); 
   let queryparams = urlsplit[1].split('&');
   let key = queryparams[queryparams.length -1].split('=');
   
   if(key[0] != 'page'){
     let newurl = prevUrl + '&page=' +page;
-    //console.log(newurl);
     getMovies(newurl);
   }
   else {
@@ -275,7 +289,6 @@ document.getElementById("current").innerText = page
 function ShowPreviousPage(page) {
  
     document.getElementById("current").innerText = page
-      //console.log('hello best friend')
    
       let urlsplit = prevUrl.split('?'); 
       let queryparams = urlsplit[1].split('&');
@@ -283,7 +296,6 @@ function ShowPreviousPage(page) {
       
       if(key[0] != 'page'){
         let newurl = prevUrl + '&page=' +page-1;
-        //console.log(newurl);
         getMovies(newurl);
       }
       else {
@@ -296,43 +308,6 @@ function ShowPreviousPage(page) {
       }
 }
 
-
-const getCast = (movie_id) =>
-{
-    let array = [];
-    let cast_array = [];
-    fetch(`https://api.themoviedb.org/3/movie/${movie_id}/credits?language=en-US&api_key=e296ec8b756d3d400034f9379f43d92f`)
-    .then( res => res.json() )
-    .then( (data) => {
-        // console.log( data.cast ) 
-        // (data.cast).map( (item) => { 
-        //     array.push( item.name)
-        // })
-        // (data.cast).map( (item , index ) => {
-        //     console.log( item.name )
-        //     array.push( item )
-        // })
-
-        for (const key in data.cast) {
-            if ((data.cast).hasOwnProperty(key)) {
-                array.push( data.cast[key].name)
-            }
-        }
-        array.map( (item) => {
-            //console.log( item )
-            cast_array= [...cast_array, item ]
-            
-        })
-        //console.log( cast_array )
-        
-    })
-    .catch( err => console.log( err ))
-    //console.log( array)
-   
-    
-   console.log(  cast_array)
-    return array
-}
 function getGenreList( gen_list )
 {
     let genre_array = []
@@ -350,3 +325,4 @@ function getGenreList( gen_list )
     return genre_array;
    
 }
+
